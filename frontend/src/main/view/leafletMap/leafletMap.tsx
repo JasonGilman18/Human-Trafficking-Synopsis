cff0276fee3df35cac4bda259ed1bf48f88fd9d4
Adam Armendariz <armenaa652@gmail.com>
2020-11-17 23:52:30 -0600

    sample database

--- frontend/src/main/view/leafletMap/leafletMap.tsx
+++ frontend/src/main/view/leafletMap/leafletMap.tsx
@@ -5,9 +5,10 @@ import { TileLayer, Marker, Rectangle, MapContainer, Circle } from 'react-leafle
 import 'leaflet/dist/leaflet.css';
 import './leafletMap.css';
 import pinIcon from './icons/pin.png';
+import '././database/countries.json';
 
 type LeafMapProps = {};
-type LeafMapStates = {mapCenter: Leaflet.LatLng};
+type LeafMapStates = { mapCenter: Leaflet.LatLng };
 const fillBlueOptions = { fillColor: 'blue' }
 const blackOptions = { color: 'black' }
 const limeOptions = { color: 'lime' }
@@ -16,18 +17,16 @@ const redOptions = { color: 'red' }
 
 class LeafMap extends React.Component<LeafMapProps, LeafMapStates>
 {
-    constructor(props: any)
-    {
+    constructor(props: any) {
         super(props);
 
         var mapCenter = Leaflet.latLng(39.8283, -98.5795);
 
-        this.state = {mapCenter: mapCenter};
+        this.state = { mapCenter: mapCenter };
     }
 
-    render() 
-    {
-        const iconMarkup = ReactDOMServer.renderToStaticMarkup(<img id="markerIcon" src={pinIcon}/>);
+    render() {
+        const iconMarkup = ReactDOMServer.renderToStaticMarkup(<img id="markerIcon" src={pinIcon} />);
         const customMarkerIcon = divIcon({
             html: iconMarkup,
             iconAnchor: Leaflet.point(12.5, 40)
@@ -35,7 +34,7 @@ class LeafMap extends React.Component<LeafMapProps, LeafMapStates>
 
         return (
             <MapContainer id="mapid" center={this.state.mapCenter} zoom={4} minZoom={4}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                 <Circle center={Leaflet.latLng(39.8283, -98.5795)} pathOptions={fillBlueOptions} radius={200} />
             </MapContainer>
         );
