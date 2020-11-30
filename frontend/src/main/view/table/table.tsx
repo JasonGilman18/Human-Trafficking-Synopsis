208160f3001c9e6f9086eb80f5d836ed07e692f9
Adam Armendariz <armenaa652@gmail.com>
2020-11-30 14:23:46 -0600

    mapping data to table

--- frontend/src/main/view/table/table.tsx
+++ frontend/src/main/view/table/table.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Table} from 'react-bootstrap';
-// import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './table.css';
 
 
 type TableViewProps = {};
-type TableViewStates = {products: any, columns: any};
+type TableViewStates = {columns: any, data: any};
 class TableView extends React.Component<TableViewProps, TableViewStates>
 {
     constructor(props: any)
@@ -14,30 +13,66 @@ class TableView extends React.Component<TableViewProps, TableViewStates>
         super(props);
 
         this.state = {
-            products: [{
-                id: 1,
-                name: "Product1",
-                price: 120
-            }, {
-                id: 2,
-                name: "Product2",
-                price: 80
-            }, {
-                id:3,
-                name: "Product3",
-                price: 30
-            }],
-            columns : [{
-                dataField: 'id',
-                text: 'Product ID'
-              }, {
-                dataField: 'name',
-                text: 'Product Name'
-              }, {
-                dataField: 'price',
-                text: 'Product Price'
-              }]
+            columns : [
+                {
+                    dataField: 'year',
+                    text: 'Year'
+                },{
+                    dataField: 'age',
+                    text: 'Age'
+                },{
+                    dataField: 'state',
+                    text: 'State'
+                },{
+                    dataField: 'region_description',
+                    text: 'Region'
+                }
+            ],
+            data: [
+                {
+                    "": "1",
+                    age: "adult",
+                    cleared: "3",
+                    clr_per_100k: "4.07030778310687",
+                    occ_per_100k: "14.9244618713918",
+                    occurrence: "11",
+                    offense: "commercial sex act",
+                    population: "737045",
+                    region: "9",
+                    region_description: "Southwest/Pacific",
+                    state: "Alaska",
+                    year: "2014",
+                    year_data: "0",
+                },
+                {
+                    "": "3",
+                    age: "adult",
+                    cleared: "0",
+                    clr_per_100k: "0",
+                    occ_per_100k: "0",
+                    occurrence: "0",
+                    offense: "commercial sex act",
+                    population: "38280824",
+                    region: "9",
+                    region_description: "Southwest/Pacific",
+                    state: "California",
+                    year: "2014",
+                    year_data: "0"
+                },
+            ]
         }
+    }
+
+    renderData = (data: any, index: any) => {
+        return(
+            <tr key = {index}>
+                <td>{data.year}</td>
+                <td>{data.age}</td>
+                <td>{data.state}</td>
+                <td>{data.region_description}</td>
+
+            </tr>
+        )
     }
 
     render()
@@ -45,153 +80,18 @@ class TableView extends React.Component<TableViewProps, TableViewStates>
         return (
             <div className="table">
                 <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</Table>
+                    <thead>
+                        <tr>
+                            <th>Year</th>
+                            <th>Age</th>
+                            <th>State</th>
+                            <th>Region</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.data.map(this.renderData)}
+                    </tbody>
+                </Table>
             </div>
         );
     }
